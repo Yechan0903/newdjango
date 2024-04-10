aea84d2c751e45009256c9b419a569c924028c41
신예찬 <oolo9876@yonsei.ac.kr>
2024-04-11 00:51:39 +0900

    anjdla

--- lionproject/blog/views.py
+++ lionproject/blog/views.py
@@ -9,12 +9,12 @@ def home(request):
     count = len(blogs)
     return render(request, 'home.html', {'blogs':blogs,'count':count})
 
-def detail(request, id,t):
+def detail(request, id):
     blog = get_object_or_404(Blog, pk = id)
-    return render(request, 'detail.html', {'blog':blog,'id':id,'t':t})
+    return render(request, 'detail.html', {'blog':blog,'id':id})
 
-def new(request ,a):
-    return render(request,'new.html',{'a':a})
+def new(request):
+    return render(request,'new.html')
 
 def create(request):
     new_blog = Blog()
@@ -24,3 +24,21 @@ def create(request):
     new_blog.pub_date = timezone.now()
     new_blog.save()
     return redirect('detail',new_blog.id)
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id = id)
+    return render(request,'edit.html',{'blog':edit_blog})
+
+def update(request ,id):
+    update_blog = Blog.objects.get(id=id)
+    update_blog.title = request.POST['title']
+    update_blog.writer = request.POST['writer']
+    update_blog.body = request.POST['body']
+    update_blog.pub_date = timezone.now()
+    update_blog.save()
+    return redirect('detail', update_blog.id)
+
+def delete(request , id):
+    delete_blog = Blog.objects.get(id=id)
+    delete_blog.delete()
+    return redirect('home')
--- lionproject/lionproject/urls.py
+++ lionproject/lionproject/urls.py
@@ -21,7 +21,10 @@ from blog.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
-    path('<str:id>/<str:t>',detail, name = "detail"),
-    path('new/<str:a>',new,name='new'),
+    path('new/',new,name="new"),
     path('create/',create,name = "create"),
+    path('<str:id>/',detail, name = "detail"),
+    path('edit/<str:id>',edit, name='edit'),
+    path('update/<str:id>',update, name="update"),
+    path('delete/<str:id>' , delete, name='delete'),
 ]
